a960de3621367921b3ba3e77789d37d767e399f2
Islam Madkaur <islammadkaur@gmail.com>
2020-10-03 22:14:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -11,7 +11,6 @@ def state_log(star_date)
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
-  binding.pry
+  puts state_log(date)
 end
