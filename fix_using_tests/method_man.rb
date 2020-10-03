6230964169f0de91af532009308999a5aa6cde1f
Islam Madkaur <islammadkaur@gmail.com>
2020-10-03 21:54:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,11 +4,11 @@ def start_game(player1, player2)
   player1 = "Rocky"
   playar2 = "Bullwinkle"
   "Hello " player1 "& " playar2
-  binding.pry
+  #binding.pry
 end
 
 def play_game(player1, player2)
   player1 " is better than " player2
-  binding.pry
+  #binding.pry
 
 end
